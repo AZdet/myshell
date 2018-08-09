b080e0ec5ea6a92d07a62758f1a89bf4c797eb3b
Alex Zhou <3150103078@zju.edu.cn>
2018-08-10 00:06:49 +0800

    update test

--- test.c
+++ test.c
@@ -4,136 +4,217 @@
 #include <sys/stat.h>
 #include "test.h"
 #define MAX_LEN 100
-//  
+//
 //
 char **terms;
 int idx = 1;
-char *getNextStr(){
-    return terms[idx++];
+char *getNextStr()
+{
+    static int over = 0;
+    if (over){
+        fprintf(stderr, "invalid test expression\n");
+        exit(1);
+    }
+    if (terms[idx])
+        return terms[idx++];
+    else{
+        over = 1;
+        return NULL;
+    }
+        
 }
-char *checkNextStr(){
+char *checkNextStr()
+{
     return terms[idx];
 }
-int eval_term(){
+int eval_term()
+{
     char *str, str2, str3;
     int ret;
     struct stat buf;
     str = getNextStr();
-    if (!strcmp(str, "-d")){
+    if (!strcmp(str, "-d"))
+    {
         str2 = getNextStr();
-        stat(str2, &buf);
+        ret = stat(str2, &buf);
+        if (ret)
+        {
+            perror("stat in test");
+            exit(1);
+        }
         return S_ISDIR(buf.st_mode);
     }
-    else if (!strcmp(str, "-f")){
-       // S_ISREG
-    }
-    else if (!strcmp(str, "-r")){
-         // access
-    }
-    else if (!strcmp(str, "-s")){
+    else if (!strcmp(str, "-f"))
+    {
+        // S_ISREG
         str2 = getNextStr();
-        stat(str2, &buf);
-        return buf.st_size > 0;    
+        ret = stat(str2, &buf);
+        if (ret)
+        {
+            perror("stat in test");
+            exit(1);
+        }
+        return S_ISREG(buf.st_mode);
     }
-    else if (!strcmp(str, "-w")){
-         // access
-    }
-    else if (!strcmp(str, "-x")){
-         // access
-    }
-    else if (!strcmp(str, "-b")){
-        //S_ISBLK
-    }
-    else if (!strcmp(str, "-c")){
-        //S_ISCHR
-    }
-    else if (!strcmp(str, "-e")){ // exists
+    else if (!strcmp(str, "-r"))
+    {
         // access
     }
-    else if (!strcmp(str, "-L")){
-        // S_ISLNK
-    }
-    else if (!strcmp(str, "-n")){
-        
-    }
-    else if (!strcmp(str, "-z")){
-        
-    }
-    else {
+    else if (!strcmp(str, "-s"))
+    {
         str2 = getNextStr();
-        if (!str2){
-
+        stat(str2, &buf);
+        return buf.st_size > 0;
+    }
+    else if (!strcmp(str, "-w"))
+    {
+        // access
+    }
+    else if (!strcmp(str, "-x"))
+    {
+        // access
+    }
+    else if (!strcmp(str, "-b"))
+    {
+        //S_ISBLK
+        str2 = getNextStr();
+        ret = stat(str2, &buf);
+        if (ret)
+        {
+            perror("stat in test");
+            exit(1);
         }
-        else if (!strcmp(str2, "-eq")){
-
+        return S_ISBLK(buf.st_mode);
+    }
+    else if (!strcmp(str, "-c"))
+    {
+        str2 = getNextStr();
+        ret = stat(str2, &buf);
+        if (ret)
+        {
+            perror("stat in test");
+            exit(1);
         }
-        else if (!strcmp(str2, "-ge")){
-            
+        return S_ISCHR(buf.st_mode);
+        //S_ISCHR
+    }
+    else if (!strcmp(str, "-e"))
+    { // exists
+        // access
+    }
+    else if (!strcmp(str, "-L"))
+    {
+        // S_ISLNK
+        str2 = getNextStr();
+        ret = stat(str2, &buf);
+        if (ret)
+        {
+            perror("stat in test");
+            exit(1);
         }
-        else if (!strcmp(str2, "-gt")){
-            
+        return S_ISLNK(buf.st_mode);
+    }
+    else if (!strcmp(str, "-n"))
+    {
+        str2 = getNextStr();
+        return strlen(str2) > 0;
+    }
+    else if (!strcmp(str, "-z"))
+    {
+        str2 = getNextStr();
+        return strlen(str2) == 0;
+    }
+    else
+    {
+        str2 = getNextStr();
+        if (!str2)
+        {
+            return !strcmp(str, "");
         }
-        else if (!strcmp(str2, "-le")){
-            
+        else
+        {
+            str3 = getNextStr();
+            if (!strcmp(str2, "-eq"))
+            {
+            }
+            else if (!strcmp(str2, "-ge"))
+            {
+            }
+            else if (!strcmp(str2, "-gt"))
+            {
+            }
+            else if (!strcmp(str2, "-le"))
+            {
+            }
+            else if (!strcmp(str2, "-lt"))
+            {
+            }
+            else if (!strcmp(str2, "-ne"))
+            {
+            }
+            else if (!strcmp(str2, "="))
+            {
+            }
+            else if (!strcmp(str2, "!="))
+            {
+            }
         }
-        else if (!strcmp(str2, "-lt")){
-            
-        }
-        else if (!strcmp(str2, "-ne")){
-            
-        }
-        else if (!strcmp(str2, "=")){
-            
-        }
-        else if (!strcmp(str2, "!=")){
-            
-        }
-        
     }
 }
-int eval_expr(){
+int eval_expr()
+{
     int ret1, ret2, res, not = 0;
     char *str;
-    if (!strcmp(checkNextStr(), "!")){
+    if (!strcmp(checkNextStr(), "!"))
+    {
         not = 1;
         getNextStr();
     }
     ret1 = eval_term();
-    if (ret1 == -1){
+    if (ret1 == -1)
+    {
         fprintf(stderr, "Error: invalid test expression\n");
         return -1;
     }
     str = getNextStr();
-    if (!str){
+    if (!str)
+    {
         res = ret1;
     }
-    else if (!strcmp(str, "-a")){
+    else if (!strcmp(str, "-a"))
+    {
         ret2 = eval_expr();
-        if (ret2 == -1){
+        if (ret2 == -1)
+        {
             fprintf(stderr, "Error: invalid test expression\n");
             return -1;
         }
         res = ret1 && ret2;
     }
-    else if (!strcmp(str, "-o")){
+    else if (!strcmp(str, "-o"))
+    {
         ret2 = eval_expr();
-        if (ret2 == -1){
+        if (ret2 == -1)
+        {
             fprintf(stderr, "Error: invalid test expression\n");
             return -1;
         }
         res = ret1 || ret2;
     }
-    else {
+    else
+    {
         fprintf(stderr, "Error: invalid test expression\n");
         return -1;
     }
     return (not) ? !res : res;
 }
-int do_test(char **cmd_argv){
+int do_test(char **cmd_argv)
+{
     terms = cmd_argv;
     return eval_expr();
 }
-int main(){
+int main()
+{
     // char str[MAX_LEN];
     // gets(str);
     // puts(getenv(str));
